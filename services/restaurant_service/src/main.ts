6d33f06a0ee10a55e036ca884006545d34ddc7a6
Moshizzl3 <81045658+Moshizzl3@users.noreply.github.com>
2024-11-18 14:55:05 +0100

    added docker and docker compose

--- services/restaurant_service/src/main.ts
+++ services/restaurant_service/src/main.ts
@@ -1,6 +1,6 @@
 import { app } from "~/app";
 
-app.listen({ host: "127.0.0.1", port: 5000 }, (err, addr) => {
+app.listen({ host: "0.0.0.0", port: 3000 }, (err, addr) => {
   if (err) {
     console.log(err);
     process.exit(1);
